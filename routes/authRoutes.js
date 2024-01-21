a60a374c3b8a96c9fe2f03bf2900ba0a667b86a4
KIRAN <88279441+kiran2023@users.noreply.github.com>
2024-01-21 17:21:59 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.route('/signup').get(authController.userVerification, authController.fetchRegisteredUsers).post(authController.userRegistration);
-router.route('/login').post(authController.login);
+router.route('/signup').get(authController.userVerification, authController.fetchRegisteredUsers).post(authController.userVerification, authController.userRegistration);
+router.route('/login').post(authController.userVerification, authController.login);
 
-router.route('/forgotPassword').post(authController.forgotPassword);
-router.route('/resetPassword/:token').patch(authController.resetPassword);
+router.route('/forgotPassword').post(authController.userVerification, authController.forgotPassword);
+router.route('/resetPassword/:token').patch(authController.userVerification,authController.resetPassword);
 
 module.exports = router;
